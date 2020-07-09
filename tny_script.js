e7b0b18a458bce63ce3fe50fa93dac5a3fd53cb8
LPisano721 <66844038+LPisano721@users.noreply.github.com>
2020-07-09 15:00:51 -0400

    Add files via upload
    initial commit

---
+++ tny_script.js
@@ -0,0 +1,45 @@
+/*
+   Countdown Clock
+   Author: Luigi Pisano	
+   Date:   July 6, 2020
+
+*/
+
+//Execute the funtion the run and display the countown clock.
+runClock();
+
+//Repeat the call to the runClock() function every second
+
+setInterval ("runClock()", 1000);
+
+//Function definition fro runClock()
+function runClock() {
+	// Store the current date and time
+	var currentDay= new Date();
+	var dateStr= currentDay.toLocaleDateString();
+	var timeStr= currentDay.toLocaleTimeString();
+
+	// Display the current date and time.
+	document.getElementById("dateNow").innerHTML= dateStr+ "<br/>"+timeStr;
+
+	// Calculate the days until January 1st
+	var newYear= new Date("January 1, 2018");
+	var nextYear= currentDay.getFullYear()+1;
+	newYear.setFullYear(nextYear);
+	var daysLeft= (newYear-currentDay)/(1000*60*60*24);
+
+	//Calculate the days in the current day
+	var hrsLeft= (daysLeft-Math.floor(daysLeft))*24;
+
+	//Calculate the minutes and seconds in the current day
+	var minsLeft=(hrsLeft-Math.floor(hrsLeft))*60;
+	var secsLeft=(minsLeft-Math.floor(minsLeft))*60;
+
+	// Display the time left until New Year's Eve
+	document.getElementById("days").textContent= Math.floor(daysLeft); 
+	document.getElementById("hrs").textContent= Math.floor(hrsLeft);
+	document.getElementById("mins").textContent= Math.floor(minsLeft);
+	document.getElementById("secs").textContent= Math.floor(secsLeft);
+
+return};
+
